3d5d75b52258c52ac10a4a7e515dee46156f1856
harnesha <jayharnesha@outlook.com>
2022-04-03 11:10:40 +0530

    setting.py file is changed

--- lunchtime/lunchtime/settings.py
+++ lunchtime/lunchtime/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1g%+e0cb==kr-z=qk&7$=&savu9*xamm!)!m*h490g018c=s39'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
