4d3e403fb22684fa35e494b4159ee38f13fe85e2
Sunny Yadav <sunny.y@simformsolutions.com>
2022-04-03 14:15:14 +0530

    Done completing

--- lunchtime/menu/views.py
+++ lunchtime/menu/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from rest_framework import status
+
 from .serializer import MenuSerializer
 from rest_framework.decorators import api_view
 # Create your views here.
@@ -14,43 +16,41 @@ from types import SimpleNamespace
 # class MenuView(ModelViewSet):
 #     queryset = User.objects.all()
 #     serializer_class = MenuSerializer
-@api_view(["POST", "GET"])
+@api_view(["POST"])
 def createMenu(request):
     a = request.data.get("menu")
-    print(a)
     a = a[1:len(a) - 1]
     d = eval(a)
-
-    print(d)
     b = 0
     c = {}
     for i in d:
         c[b] = i
         b += 1
-    print(c)
-    # print(a[0])
-    print(c[0])
     z = c[0]
-    print(z)
-
     menu_obj = Menu.objects.create(menu=z, latest_date=request.data.get("time"))
     print(menu_obj)
     serializer = MenuSerializer(menu_obj, data=request.data)
     print(serializer)
     if serializer.is_valid():
+        print("IN")
         serializer.save()
         return Response(c)
     return Response(serializer.errors)
 
 
 @api_view(["GET"])
-def getMenu(request,pk):
-    data = Menu.objects.filter(latest_date=pk).first()
-    a = eval(data.menu)
-    b = {}
-    c = 0
-    for i in a:
-        b[c] = i
-        c += 1
-    serializer = MenuSerializer(data)
-    return Response(b)
+def getMenu(request, pk):
+    try:
+        data = Menu.objects.filter(latest_date=pk).first()
+        if data:
+            a = eval(data.menu)
+            b = {}
+            c = 0
+            for i in a:
+                b[c] = i
+                c += 1
+            serializer = MenuSerializer(data)
+            return Response(b)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    except BaseException as e:
+        print(e)
